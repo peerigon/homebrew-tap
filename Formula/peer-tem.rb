04ac07142bf58ce37355e880545b60285575429b
Niklas Haug <niklas.haug@peerigon.com>
2026-03-06 14:09:00 +0100

    update peer-tem formula with newly released version of peer-tem v1.2.1

--- Formula/peer-tem.rb
+++ Formula/peer-tem.rb
@@ -1,9 +1,9 @@
 class PeerTem < Formula
   desc "Peerigon Peer TEM [Travel Expense Manager]"
   homepage "https://github.com/peerigon/peer-tem"
-  url "https://github.com/peerigon/peer-tem/releases/download/v1.2.0/peer-tem-macos-arm64.zip"
-  sha256 "cbd8a1ddbbf3a20fb56eec16caa441700f3c4d2f53826f3eb4acc4c24a94345a"
-  version "1.2.0"
+  url "https://github.com/peerigon/homebrew-tap/releases/download/v1.0.0/peer-tem-macos-arm64.zip"
+  sha256 "d090e1230b858d24f37c4f47516f4a576c2d5d0fd4c2e7e917c62aaa7420fc24"
+  version "1.2.1"
 
   def install
     bin.install "peer-tem-macos-arm64" => "peer-tem"
